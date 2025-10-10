fc3a7a0cb20cb00ee01b4961f375e3b2e18208ab
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-10-10 10:41:26 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -14,13 +14,14 @@ function menuShow() {
 
 }
 
-    let botao_1 = document.querySelector('.botao_1');
-    let botao_2 = document.querySelector('.botao_2');
-    let botao_3 = document.querySelector('.botao_3');
+// FUNÇÕES DO SIDBAR CLIENTE
+let botao_1 = document.querySelector('.botao_1');
+let botao_2 = document.querySelector('.botao_2');
+let botao_3 = document.querySelector('.botao_3');
 
-    let secao_1 = document.querySelector('.secao_1');
-    let secao_2 = document.querySelector('.secao_2');
-    let secao_3 = document.querySelector('.secao_3');
+let secao_1 = document.querySelector('.secao_1');
+let secao_2 = document.querySelector('.secao_2');
+let secao_3 = document.querySelector('.secao_3');
 
 function sidbar_minhas_reservas() {
     
@@ -72,3 +73,62 @@ function sidbar_perfil() {
         secao_2.classList.add('ocuto');
     }
 }
+
+// FUNÇÕES DO MENU AMBURGUE CLIENTE
+let botaoMenuBurger_1 = document.querySelector('.botaoMenuBurger_1');
+let botaoMenuBurger_2 = document.querySelector('.botaoMenuBurger_2');
+let botaoMenuBurger_3 = document.querySelector('.botaoMenuBurger_3');
+
+function sidbarMenu_reservas() {
+    
+    if (!botaoMenuBurger_1.classList.contains('select-menu')) {
+        botaoMenuBurger_1.classList.add('select-menu');
+        botaoMenuBurger_2.classList.remove('select-menu'); 
+        botaoMenuBurger_3.classList.remove('select-menu');
+
+        secao_1.classList.remove('ocuto');
+        secao_1.classList.add('minhas-reservas');
+
+        secao_2.classList.remove('minhas-reservas');
+        secao_2.classList.add('ocuto');
+        secao_3.classList.remove('minhas-reservas');
+        secao_3.classList.add('ocuto');
+    }
+} 
+
+function sidbarMenu_agendamento() {
+    if (!botaoMenuBurger_2.classList.contains('select-menu')) {
+
+        botaoMenuBurger_2.classList.add('select-menu');
+        botaoMenuBurger_1.classList.remove('select-menu'); 
+        botaoMenuBurger_3.classList.remove('select-menu');
+
+        secao_2.classList.remove('ocuto');
+        secao_2.classList.add('minhas-reservas');
+
+        secao_1.classList.remove('minhas-reservas');
+        secao_1.classList.add('ocuto');
+        secao_3.classList.remove('minhas-reservas');
+        secao_3.classList.add('ocuto');
+    }
+}
+
+function sidbarMenu_perfil() {
+    if (!botaoMenuBurger_3.classList.contains('select-menu')) {
+
+        botaoMenuBurger_3.classList.add('select-menu');
+        botaoMenuBurger_1.classList.remove('select-menu'); 
+        botaoMenuBurger_2.classList.remove('select-menu');
+
+        secao_3.classList.remove('ocuto');
+        secao_3.classList.add('minhas-reservas');
+
+        secao_1.classList.remove('minhas-reservas');
+        secao_1.classList.add('ocuto');
+        secao_2.classList.remove('minhas-reservas');
+        secao_2.classList.add('ocuto');
+    }
+}
+
+
+
